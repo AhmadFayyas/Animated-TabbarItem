8fe13f6920ef3df563da08c7354ad15b6722d4dc
Ahmad Fayyas <ahmadfayyas@acd2670b.ipt.aol.com>
2018-10-24 13:24:29 -0400

    Initial Commit

---
+++ answer/CustomTabbarController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTabbarController.swift
+//  answer
+//
+//  Created by Ahmad Fayyas on 10/24/18.
+//  Copyright © 2018 Ahmad Fayyas. All rights reserved.
+//
+
+import Foundation
--- answer/FirstViewController.swift
+++ answer/FirstViewController.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tabBarItem.tag = TabbarItemTag.firstViewController.rawValue
     }
-
-
 }
 
